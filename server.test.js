2d23e3d42de64abd608a44b78c2d43803a78fd81
Ihor Mykhno <igormykhno@gmail.com>
2021-07-27 18:39:28 +0300

    Commit

--- server.test.js
+++ server.test.js
@@ -1,12 +1,12 @@
 import * as emp from './server.js';
 
-let map = new Map([
-    {0: 1},
-    {1: 3},
-]);
+let map = new Map();
 
 describe('Check who win', () => {
     it('if everyone chooses the same', () => {
-        map.set(0, 1)
+        map.set(0, 1);
+        map.set(1, 1);
+        map.set(2, 1);
+       expect(emp.funcWhoWin(map)).toBe
     });
 });
